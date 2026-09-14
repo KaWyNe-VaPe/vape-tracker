181233fe8fb457f0fadd59e903d8f255067c4dc8
KaWyNe-VaPe <karine.n.lopez@gmail.com>
2026-09-14 22:43:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
+// Service Worker pour la gestion des notifications PWA
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+});
 
+self.addEventListener('activate', (e) => {
+    return self.clients.claim();
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(windowClients => {
+            for (let i = 0; i < windowClients.length; i++) {
+                let client = windowClients[i];
+                if ('focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow('/');
+            }
+        })
+    );
+});
